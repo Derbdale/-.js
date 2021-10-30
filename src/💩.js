a35920eb888cfc41dfe1a96ceec356a26a02f063
Ulises Viña <uvina06@gmail.com>
2021-10-29 21:32:53 -0500

    Update 💩.js
    Literally made the code way cleaner.

--- src/💩.js
+++ src/💩.js
@@ -1,26 +1,17 @@
 export default {
-    "💩": {
-        ieVersion: function() {
-            let ua = window.navigator.userAgent;
+    "💩": {
+        ieVersion: _ => {
+            let ua = window.navigator.userAgent,
+            msie = ua.indexOf("MSIE "),
+            trident = ua.indexOf("Trident/"),
+            edge = ua.indexOf('Edge/');;
 
-            let msie = ua.indexOf('MSIE ');
-            if (msie > 0) {
-                return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-            }
-
-            let trident = ua.indexOf('Trident/');
-            if (trident > 0) {
-                let rv = ua.indexOf('rv:');
-                return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-            }
-
-            let edge = ua.indexOf('Edge/');
-            if (edge > 0) {
-                return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-            }
-            return false;
+            if(msie > 0 ) return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+            if(trident > 0) { let rv = ua.indexOf("rv:"); return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10); }
+            if (edge > 0) return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+            return null;
         },
-        isIE: function(){
+        isIE: _ => {
             return this.ieVersion() !== false;
         }
     }
